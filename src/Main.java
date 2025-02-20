5746973c7a5cebc96b8be8395d0535c1b0654010
Javokhirbek Ruziboev <159406468+Javokhir05@users.noreply.github.com>
2025-02-20 22:33:08 +0100

    Second changes

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,20 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
 
+        //This is my first code in Java
+
+        // By using for loop!!!
         for(int i = 0; i < 7; i ++){
             System.out.println("Hello Vistula" + i);
         }
+        //-----
+
+        int age = 19;
+        System.out.println(age);
+        int year = 2025;
+        int month = 2;
+        int day = 20;
+        System.out.println("the code was written in " + day+"/"+month+"/"+year);
+
     }
 }
